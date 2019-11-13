a6b9f15eb8713ab76a6248fa23993ed4084a3bf8
xyAshay <ashay.mailbox@gmail.com>
2019-11-13 19:46:59 +0530

    Basic Functions Added

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 	"gopkg.in/alecthomas/kingpin.v2"
@@ -14,15 +15,44 @@ var (
 	dev  = kingpin.Flag("dev", "Enable Developer Mode").Short('d').Bool()
 )
 
-func getLanding(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Bucket Landing</h1>")
+type serverInfo struct {
+	root   string
+	router *mux.Router
+}
+
+func createServer(root string) *serverInfo {
+	router := mux.NewRouter()
+	if root == "" {
+		root = "."
+	}
+	init := &serverInfo{
+		root:   root,
+		router: router,
+	}
+	log.Printf("Root : %s\n", init.root)
+	router.HandleFunc("/", init.getLanding)
+	router.HandleFunc("/serve/{path:.*}", init.getPath)
+	return init
+}
+
+func (info *serverInfo) getLanding(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Bucket Landing</h1> <br> <a href='/serve/'>Get Started</a>")
+}
+
+func (info *serverInfo) getPath(w http.ResponseWriter, r *http.Request) {
+	path := mux.Vars(r)["path"]
+	log.Println("Current Path ", info.root, path)
+	http.ServeFile(w, r, filepath.Join(info.root, path))
+}
+
+func (info *serverInfo) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	info.router.ServeHTTP(w, r)
 }
 
 func main() {
 	kingpin.Parse()
-	app := mux.NewRouter()
+	app := createServer("./")
 
-	app.HandleFunc("/", getLanding)
-	fmt.Printf("Server Running on https://localhost:%s \nDeveloper : %t", *port, *dev)
+	fmt.Printf("Server Running on https://localhost%s \nDeveloper : %t\n", *port, *dev)
 	log.Fatal(http.ListenAndServe(*port, app))
 }
