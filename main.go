44bca0c43774294f43b84459e4d39fffbfd1f66a
xyAshay <ashay.mailbox@gmail.com>
2019-12-15 16:07:36 +0530

    Now Fetching Files + Added VueJS

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 
 	"github.com/gorilla/mux"
@@ -20,6 +22,14 @@ type serverInfo struct {
 	router *mux.Router
 }
 
+//FileListing : Generate File List JSON
+type FileListing struct {
+	Name string `json:"name"`
+	Path string `json:"path"`
+	Type string `json:"type"`
+	Size string `json:"size"`
+}
+
 func createServer(root string) *serverInfo {
 	router := mux.NewRouter()
 	if root == "" {
@@ -32,17 +42,54 @@ func createServer(root string) *serverInfo {
 	log.Printf("Root : %s\n", init.root)
 	router.HandleFunc("/", init.getLanding)
 	router.HandleFunc("/serve/{path:.*}", init.getPath)
+	router.HandleFunc("/api/json/{path:.*}", init.getJSONlisting)
 	return init
 }
 
 func (info *serverInfo) getLanding(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Bucket Landing</h1> <br> <a href='/serve/'>Get Started</a>")
+	http.ServeFile(w, r, "./web/index.html")
 }
 
 func (info *serverInfo) getPath(w http.ResponseWriter, r *http.Request) {
 	path := mux.Vars(r)["path"]
 	log.Println("Current Path ", info.root, path)
-	http.ServeFile(w, r, filepath.Join(info.root, path))
+	http.ServeFile(w, r, "./web/serve.html")
+	//http.ServeFile(w, r, filepath.Join(info.root, path))
+}
+
+func (info *serverInfo) getJSONlisting(w http.ResponseWriter, r *http.Request) {
+	path := mux.Vars(r)["path"]
+	list := make([]FileListing, 0)
+	fd, err := os.Open(filepath.Join(info.root, path))
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	defer fd.Close()
+
+	files, err := fd.Readdir(-1)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	for _, file := range files {
+		newFile := FileListing{
+			Name: file.Name(),
+			Path: filepath.Join(path, file.Name()),
+		}
+		if file.IsDir() {
+			newFile.Type = "dir"
+			newFile.Size = "-1"
+		} else {
+			newFile.Type = "file"
+			newFile.Size = "0"
+		}
+		list = append(list, newFile)
+	}
+	data, _ := json.Marshal(list)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
 }
 
 func (info *serverInfo) ServeHTTP(w http.ResponseWriter, r *http.Request) {
